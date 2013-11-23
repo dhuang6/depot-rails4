9d6545f7d40f7688642b879e7c0217251852fdb1
dhuang6 <dhuang684@gmail.com>
2013-11-23 10:47:53 -0600

    Building a shopping cart app

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '66203f8a79e4bfbf76bbfdd6c9810fa30ef632c1e372c433c78c12e79cfdfcf5cd99ccaa5332bcde9ea280f13adab919101da66d1cb9010b947fde5a7888a57b'
