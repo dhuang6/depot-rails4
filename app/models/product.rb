1c2a9efc1ac9335eb0dcca379fae50743bd35bf9
dhuang6 <dhuang684@gmail.com>
2013-11-23 23:59:46 -0600

    more progress

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,5 +6,23 @@ class Product < ActiveRecord::Base
 	validates :image_url, allow_blank: true, format: {
 			  with: %r{\.(gif|jpg|png)\Z}i,
 			  message: 'must be a URL for GIF, JPG or PNG image.'
-			  }
-end
+		       }
+			   
+	def self.latest
+		Product.order(:updated_at).last
+end		
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+	private
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+				return false
+			end		   
+		end
+    end
